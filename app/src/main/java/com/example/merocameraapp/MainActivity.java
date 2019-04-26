5ea89b44a3b33843ac3004b8efd3922259d32b09
Softwarica College Lab <softwarica@softwarica.com>
2019-04-26 08:30:53 +0545

    Caera pic was saved to internal storage

--- app/src/main/java/com/example/merocameraapp/MainActivity.java
+++ app/src/main/java/com/example/merocameraapp/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.merocameraapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button_image;
+    private ImageView imageview;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_image = findViewById(R.id.button_image);
+        imageview = findViewById(R.id.imageview);
+        checkPermission();
+
+        button_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadCamera();
+            }
+        });
+
+    }
+
+    private void checkPermission()
+    {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {
+                            Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    },0);
+        }
+    }
+
+    private void loadCamera()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, 1); ///khi result expect gareko camera app bata
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode ==RESULT_OK)
+        {
+            Bundle extras = data.getExtras(); //Euta Activity bata arko Activity ma pathaunu ko lagi
+            Bitmap imageBitmap = (Bitmap) extras.get("data"); //image bitmap ma aaune vaeko le
+            imageview.setImageBitmap(imageBitmap); //setImageBitmap le image set garcha
+        }
     }
 }
